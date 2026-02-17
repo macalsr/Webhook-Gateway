d24c66d51977e2c6e4bb9d3eb4fc2277d00a268c
Maria <mmariasribeiro@gmail.com>
2026-02-17 11:47:50 -0300

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/test/java/com/mariaribeiro/webhookprocessor/WebhookProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariaribeiro.webhookprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
